e490516a70445d9cd2a941862f9a068426764873
unknown <sbarre33@gmail.com>
2020-07-21 13:37:24 -0600

    declared second <ul> for completedList and matched the elements.

--- js/script.js
+++ js/script.js
@@ -4,17 +4,16 @@
 
 To do this in JS, 
 1. declare variables for input, button, list item and list */
-let input = document.querySelector("addItem");
-let button = document.querySelector("addBtn");
-let item = document.querySelector("li");
-let myList = document.querySelector("myList");
-console.log("addItem");
-console.log("addBtn");
-console.log("li");
-console.log("myList");
+let input = document.getElementById("addItem");
+let button = document.querySelector("#addBtn");
+let myList = document.getElementById("myList");
+let completedList = document.getElementById("completedList")
+console.log(input);
+console.log(button);
+console.log(myList);
 
 //Create an EventListener
-button.addEventListener("click", addToMyList) //ERROR: Uncaught TypeError: Cannot read property 'addEventListener' of null
+button.addEventListener("click", addToMyList) 
 function addToMyList(event){
     event.preventDefault()
     console.log("button pressed!");
@@ -24,17 +23,17 @@ function addToMyList(event){
         moveToCompletedList(newItem);         //calling a function by name which we passed the item through
 
     });
-/*Next, if we want to add a text to <li> element, we need to create a text node. We then need to add input value inside text node so that
-whenever we enter an input inside a function, it produces an output.*/
-let textNode=document.createTextNode(input.value);
-// Next, we prepend the text to <li>
-newItem.prepend(textNode);
-//then, prepend the newItem element to existing element
-myList.prepend(newItem);
+    /*Next, if we want to add a text to <li> element, we need to create a text node. We then need to add input value inside text node so that
+    whenever we enter an input inside a function, it produces an output.*/
+    let textNode=document.createTextNode(input.value); //***************uncaught typeError: Cannot read input... */
+    // Next, we prepend the text to <li>
+    newItem.prepend(textNode);
+    //then, prepend the newItem element to existing element
+    myList.prepend(newItem); //*********************Uncaught TypeError: Cannot read property 'prepend' of null
 }
-//create a function and use prepend method to move to item from myList to CompletedList
-function moveToCompletedList(item){
-    moveToCompletedList.prepend(newItem);
+//create a function to move to item from myList to CompletedList
+function moveToCompletedList(newItem){
+    completedList.prepend(newItem);
 } 
 
 
